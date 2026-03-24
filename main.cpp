2688e5139889bd5b10799246f7abf707bb398122
Letong He <85004963+gcAfanGer@users.noreply.github.com>
2026-03-24 18:06:24 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int getEnter() {
+    char dummy;
+    while (cin >> dummy) {
+        getchar();
+        if (dummy == 'n') {
+            return 0;
+        }
+        if (dummy == 'Q') {
+            return 1;
+        }
+    }
+}
+
+int main() {
+    ifstream Fil;
+    Fil.open("../present.txt");
+    // if (!Fil.is_open()) {
+    //     cout << "Could not open file" << endl;
+    // }
+
+    string str;
+    while (getline(Fil, str)) {
+        cout << str << endl;
+        if (getEnter()==1) {
+            cout << "skipping the text file";
+            Fil.close();
+            break;
+        }
+    }
+
+
+
+}
